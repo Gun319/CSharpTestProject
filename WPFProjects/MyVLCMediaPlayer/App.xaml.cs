73388caf3a37d673f1f262f521945e3924372be6
Gun319 <junjie_xmail@foxmail.com>
2023-05-07 10:51:06 +0800

    优化代码

--- WPFProjects/MyVLCMediaPlayer/App.xaml.cs
+++ WPFProjects/MyVLCMediaPlayer/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using LibVLCSharp.Shared;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace MyVLCMediaPlayer
@@ -8,7 +10,29 @@ namespace MyVLCMediaPlayer
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// 窗口切换至最前
+        /// </summary>
+        /// <param name="hWnd">窗口句柄</param>
+        /// <param name="fAltTab">是否切换至最前</param>
+        [DllImport("user32.dll")]
+        private static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+
         public App()
+        {
+            InitializeComponent();
+
+            Process[] pList = Process.GetProcessesByName("MyVLCMediaPlayer");
+
+            if (pList.Where(p => p.Id != Environment.ProcessId).Any())
+            {
+                SwitchToThisWindow(pList.FirstOrDefault().MainWindowHandle, true);
+
+                Environment.Exit(0);
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
         {
             Task.Factory.StartNew(() =>
             {
